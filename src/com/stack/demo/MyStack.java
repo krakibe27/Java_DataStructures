7928a2bcaef436987d84d17a15ea158b7fe4a0c9
Kapil P Rakibe <kapil@Kapils-MacBook-Pro.local>
2017-05-02 20:26:08 -0700

    All Added

--- src/com/stack/demo/MyStack.java
+++ src/com/stack/demo/MyStack.java
@@ -1,5 +1,21 @@
 package com.stack.demo;
 
+
+class Stack {
+	
+	private int top = -1;
+	private int array[];
+	
+	public void push(int s){
+		array[top++] = s;
+	}
+	
+}
+
 public class MyStack {
 
+	private int top;
+	
+	
+	
 }
